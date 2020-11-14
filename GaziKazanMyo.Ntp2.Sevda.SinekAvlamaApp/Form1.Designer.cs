08775637b706925f4c63d939cd7e076e75255194
sevda <58671217+ssimse258@users.noreply.github.com>
2020-11-14 22:19:47 +0300

    Add files via upload

---
+++ GaziKazanMyo.Ntp2.Sevda.SinekAvlamaApp/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace GaziKazanMyo.Ntp2.Sevda.SinekAvlamaApp
+{
+    partial class FrmSinek
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSinek));
+            this.sinekuretme = new System.Windows.Forms.Timer(this.components);
+            this.gerisayim = new System.Windows.Forms.Timer(this.components);
+            this.imagelist = new System.Windows.Forms.ImageList(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelsure = new System.Windows.Forms.Label();
+            this.labelskor = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sinekuretme
+            // 
+            this.sinekuretme.Interval = 1000;
+            this.sinekuretme.Tick += new System.EventHandler(this.sinekuretme_Tick);
+            // 
+            // gerisayim
+            // 
+            this.gerisayim.Interval = 1000;
+            this.gerisayim.Tick += new System.EventHandler(this.gerisayim_Tick);
+            // 
+            // imagelist
+            // 
+            this.imagelist.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imagelist.ImageStream")));
+            this.imagelist.TransparentColor = System.Drawing.Color.Transparent;
+            this.imagelist.Images.SetKeyName(0, "sinek.png");
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Maroon;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.labelsure);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.labelskor);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(884, 75);
+            this.panel1.TabIndex = 0;
+            // 
+            // labelsure
+            // 
+            this.labelsure.AutoSize = true;
+            this.labelsure.BackColor = System.Drawing.Color.Black;
+            this.labelsure.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelsure.ForeColor = System.Drawing.Color.White;
+            this.labelsure.Location = new System.Drawing.Point(330, 23);
+            this.labelsure.Name = "labelsure";
+            this.labelsure.Size = new System.Drawing.Size(26, 29);
+            this.labelsure.TabIndex = 2;
+            this.labelsure.Text = "0";
+            // 
+            // labelskor
+            // 
+            this.labelskor.AutoSize = true;
+            this.labelskor.BackColor = System.Drawing.Color.Black;
+            this.labelskor.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelskor.ForeColor = System.Drawing.Color.White;
+            this.labelskor.Location = new System.Drawing.Point(107, 23);
+            this.labelskor.Name = "labelskor";
+            this.labelskor.Size = new System.Drawing.Size(26, 29);
+            this.labelskor.TabIndex = 1;
+            this.labelskor.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(237, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "SÜRE:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SKOR:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(805, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(76, 69);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmSinek
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(884, 661);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmSinek";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sinek Avlama";
+            this.Load += new System.EventHandler(this.FrmSinek_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer sinekuretme;
+        private System.Windows.Forms.Timer gerisayim;
+        private System.Windows.Forms.ImageList imagelist;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelsure;
+        private System.Windows.Forms.Label labelskor;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ GaziKazanMyo.Ntp2.Sevda.SinekAvlamaApp/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GaziKazanMyo.Ntp2.Sevda.SinekAvlamaApp
+{
+    public partial class FrmSinek : Form
+    {
+        Button sinekbtn = new Button();
+        Random rdm = new Random();
+        static byte sure = 90;
+        static int toplam;
+        public FrmSinek()
+        {
+            InitializeComponent();
+        }
+        private void sinekuretme_Tick(object sender, EventArgs e)
+        {
+            sinekbtn = new Button();
+            sinekbtn.Size = new Size(70, 70);
+            sinekbtn.Location = new Point(rdm.Next(this.ClientSize.Width - sinekbtn.Width),rdm.Next(panel1.Height, this.ClientSize.Height - sinekbtn.Height));
+            sinekbtn.Text = rdm.Next(1, 100).ToString();
+            sinekbtn.BackgroundImage = imagelist.Images[0];
+            sinekbtn.BackgroundImageLayout = ImageLayout.Stretch;
+            sinekbtn.TextAlign = ContentAlignment.TopLeft;
+            sinekbtn.Font = new Font(sinekbtn.Font.Name, sinekbtn.Font.Size, FontStyle.Bold);
+            this.Controls.Add(sinekbtn);
+            sinekbtn.Click += Sinek_Click;
+        }
+
+        private void Sinek_Click(object sender, EventArgs e)
+        {
+            sinekbtn = (Button)sender;
+            sinekbtn.Dispose();
+            toplam += int.Parse(sinekbtn.Text);
+            labelskor.Text = toplam.ToString();
+        }
+        private void FrmSinek_Load(object sender, EventArgs e)
+        {
+            labelsure.Text = sure.ToString();
+            sinekuretme.Start();
+            gerisayim.Start();
+        }
+
+        private void gerisayim_Tick(object sender, EventArgs e)
+        {
+            sure--;
+            labelsure.Text = sure.ToString();
+            if (sure == 0)
+            {
+                gerisayim.Stop();
+                sinekuretme.Stop();
+                Skor_Kaydetme(labelskor.Text);
+                DialogResult soru = new DialogResult();
+                soru = MessageBox.Show($"Skorunuz:{labelskor.Text}\nYeniden Oynamak istermisiniz?", "Oyun Bitti", MessageBoxButtons.YesNo);
+                if (soru == DialogResult.Yes) Application.Restart();
+                else Application.Exit();
+            }
+
+        }
+
+        private void Skor_Kaydetme(string skorkayit) 
+        {
+            FileStream fs = new FileStream("skorkayit.txt", FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(DateTime.Now + " " + skorkayit + "\n");
+            fs.Flush();
+            sw.Close();
+            fs.Close();
+        }
+    }
+}
